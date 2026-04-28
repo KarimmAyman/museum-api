23942364436687085b5ba722b8dae2e0a9527d03
karim ayman <kareem.apple1111@gmail.com>
2026-04-28 15:33:02 +0300

    initial backend api

--- src/index.js
+++ src/index.js
@@ -49,5 +49,5 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`)
+    console.log(`🚀 Server running on port ${PORT}`)
 })
